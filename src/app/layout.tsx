fc1ba13071a8f87e941e9823c82104256d6a723b
Shivam <legendshivam11@gmail.com>
2025-04-14 10:28:55 +0530

    Added og:image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type React from "react";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Metadata } from "next";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +11,37 @@ export const metadata: Metadata = {
   title: "Shivam's Portfolio",
   description: "Personal portfolio website of Shivam",
   manifest: "/site.webmanifest",
+  openGraph: {
+    type: "website",
+    title: "Shivam",
+    description:
+      "A passionate developer focused on creating beautiful and functional web experiences.",
+    siteName: "Shivam",
+    images: [
+      {
+        url: `/opengraph-image.jpg`,
+        type: "image/jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shivam",
+    description:
+      "A passionate developer focused on creating beautiful and functional web experiences.",
+    creator: "@sethshivam11",
+    siteId: "765045797750706176",
+    images: [
+      {
+        url: `/opengraph-image.jpg`,
+        type: "image/jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
